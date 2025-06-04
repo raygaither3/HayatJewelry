8db593633568bde6a33149aa9ab7edd9937fb8ad
raygaither3 <raygaither3@gmail.com>
2025-06-03 21:54:16 -0500

    Call upgrade() in create_app to run migrations on deploy

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from website import create_app
+from flask_migrate import upgrade
 
 from dotenv import load_dotenv
-load_dotenv()  # Load variables from .env before anything else
-
+load_dotenv()  
 
 app = create_app()
+
+with app.app_context():
+    upgrade()
 
 if __name__ == "__main__":
     app.run(debug=True)
